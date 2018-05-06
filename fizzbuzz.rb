dd7a78c6eb017ea2aec080f117aae80f1e656278
Michael Ballard <ballard.michael9@gmail.com>
2018-05-06 15:57:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
 if num % 5 ==0 && num %3 == 0 
-  "fizzbuzz" 
+  "Fizzbuzz" 
 elsif num % 5 == 0 
-  "buzz"
+  "Buzz"
 elsif num % 3 == 0 
-  "fizz"
+  "Fizz"
 else
   "Invalid"
 end
